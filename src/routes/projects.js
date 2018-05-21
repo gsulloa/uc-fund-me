8fe7627a00064dab04a493a4dbc07aa61bffa7d1
Gabriel Ulloa <gsulloa@uc.cl>
2018-05-20 22:30:15 -0400

    New and create project routes

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -2,19 +2,45 @@ const KoaRouter = require('koa-router');
 
 const routes = new KoaRouter();
 
+
 routes.get('projects', '/', async (ctx) => {
   const projects = await ctx.orm.Project.findAll();
   return ctx.render('projects/index', {
     projects,
-    projectUrl: id => routes.url('project', { id }),
+    projectPath: id => routes.url('project', { id }),
+    newProjectPath: routes.url('newProject'),
   });
+});
+
+
+routes.get('newProject', '/new', async (ctx) => {
+  const project = await ctx.orm.Project.build();
+  return ctx.render('projects/new', {
+    project,
+    submitProjectPath: routes.url('createProject'),
+    goIndexPath: routes.url('projects'),
+  });
+});
+
+routes.post('createProject', '/', async (ctx, next) => {
+  try {
+    const project = await ctx.orm.Project.create(ctx.request.body);
+    return next();
+  } catch (e) {
+    console.log('create project error: ', e);
+    return ctx.render('projects/new', {
+      project: ctx.orm.Project.build(ctx.request.body),
+      submitProjectPath: routes.url('createProject'),
+      goIndexPath: routes.url('projects'),
+    });
+  }
 });
 
 routes.get('project', '/:id', async (ctx) => {
   const project = await ctx.orm.Project.findById(ctx.params.id);
   return ctx.render('projects/show', {
     project,
-    goIndex: routes.url('projects'),
+    goIndexPath: routes.url('projects'),
   });
 });
 
