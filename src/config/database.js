841b4d6161684a7d8e7dd3a519fdb96bf7eeb7d4
ironcadiz <afcadiz@uc.cl>
2018-05-20 22:22:45 -0400

    added more initial configs

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const config = {
   default: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME || 'pguser',
+    password: process.env.DB_PASSWORD || '123456',
     dialect: process.env.DB_DIALECT || 'postgres',
     database: process.env.DB_NAME,
     host: process.env.DB_HOST || '127.0.0.1',
