942df0a7ada85665e7bd4896410f98255310cd67
Gabriel Ulloa <gsulloa@uc.cl>
2018-05-22 17:18:37 -0400

    fix(routes): Set routes more accesibles when need (#19)
    * routes fixed
    
    * project routes
    
    * projects route in sign in/up
    
    * exposing user if signed in
    
    * showing username in navbar
    
    * project image redirect to project show
    
    * do not show project if not signed in

--- src/middlewares/addViewsFunctions.js
+++ src/middlewares/addViewsFunctions.js
@@ -2,6 +2,7 @@ const formatAsCurrency = require('../utils/currency');
 
 function addViewsFunctions(ctx, next) {
   ctx.state.formatAsCurrency = formatAsCurrency;
+  if (ctx.session.user) ctx.state.currentUser = ctx.session.user;
   return next();
 }
 
