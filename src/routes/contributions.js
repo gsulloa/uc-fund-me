b5c0c2472a09a82affe89dad0c5cb0bc319d25de
Gabriel Ulloa <gsulloa@uc.cl>
2018-06-23 21:08:04 -0400

    display errors

--- src/routes/contributions.js
+++ src/routes/contributions.js
@@ -19,15 +19,23 @@ router.get('contribution', '/', async (ctx) => {
 
 router.post('createContribution', '/', async (ctx) => {
   try {
-    const contribution = await ctx.orm.Contribution.create({
+    await ctx.orm.Contribution.create({
       amount: ctx.request.body.amount,
       UserId: ctx.session.user.id,
       ProjectId: ctx.state.project.id,
     });
-    return ctx.redirect(ctx.router.url('project', { slug: ctx.state.project.slug }));
   } catch (e) {
-    console.error(e);
+    const { project } = ctx.state;
+    const photos = await project.getImages().map(image => image.name);
+    return ctx.render('projects/show', {
+      project,
+      errors: e.errors.map(er => er.message),
+      photos: photos.map(name => ctx.router.url('imageDownload', { name })),
+      goIndexPath: ctx.router.url('projects'),
+      createContributionPath: ctx.router.url('createContribution', { slug: ctx.params.slug }),
+    });
   }
+  return ctx.redirect(ctx.router.url('project', { slug: ctx.state.project.slug }));
 });
 
 module.exports = router;
