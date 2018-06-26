70f678c410e418b2150b938558162a1beeb7a85a
Florencia Barrios <fbarrios1@uc.cl>
2018-06-26 00:39:44 -0400

    Added points system :sparkles:

--- src/middlewares/addViewsFunctions.js
+++ src/middlewares/addViewsFunctions.js
@@ -1,11 +1,18 @@
 const formatAsCurrency = require('../utils/currency');
 const moment = require('moment');
 
-function addViewsFunctions(ctx, next) {
+async function addViewsFunctions(ctx, next) {
   ctx.state.formatAsCurrency = formatAsCurrency;
   ctx.state.formatDate = dateTime => moment(dateTime).calendar();
-  if (ctx.session.user) ctx.state.currentUser = ctx.session.user;
+  if (ctx.session.user) {
+    ctx.state.currentUser = ctx.session.user;
+    ctx.state.points = Math.floor(await ctx.orm.Contribution.sum(
+      'amount',
+      { where: { UserId: ctx.session.user.id } },
+    ) * 0.005);
+  }
   return next();
 }
+
 
 module.exports = addViewsFunctions;
