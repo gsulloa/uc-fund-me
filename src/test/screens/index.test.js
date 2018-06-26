f35bb27d94b63eacec85b816b0af26760019adad
Gabriel Ulloa <gsulloa@uc.cl>
2018-06-25 20:26:39 -0400

    refactor and initial signup tests

--- src/test/screens/index.test.js
+++ src/test/screens/index.test.js
@@ -6,6 +6,11 @@ require('../../../index');
 async function clearInputs(page) {
   await page.evaluate(() => document.querySelectorAll('input').forEach((n) => { n.value = ''; })); // eslint-disable-line
 }
+async function completeForm(page, data) {
+  for (const input in data) {
+    await page.type(`input[name=${input}]`, data[input]); // eslint-disable-line
+  }
+}
 
 const APP = 'http://localhost:3000';
 let page;
@@ -43,31 +48,76 @@ describe('Sign in', () => {
     await clearInputs(page);
   });
   it('Fails with mail and password error', async () => {
-    await page.type('input[name=email]', 'no@email.com');
-    await page.type('input[name=password]', '12345');
+    await completeForm(page, {
+      email: 'no@email.com',
+      password: '12345',
+    });
     await page.click('button[type=submit]');
     const content = await page.content();
     expect(content).toContain('Email or password error');
   });
   it('Fails with mail error', async () => {
-    await page.type('input[name=email]', 'no@email.com');
-    await page.type('input[name=password]', '1234');
+    await completeForm(page, {
+      email: 'no@email.com',
+      password: '1234',
+    });
     await page.click('button[type=submit]');
     const content = await page.content();
     expect(content).toContain('Email or password error');
   });
   it('Fails with password error', async () => {
-    await page.type('input[name=email]', 'my@email.com');
-    await page.type('input[name=password]', '123');
+    await completeForm(page, {
+      email: 'my@email.com',
+      password: '12345',
+    });
     await page.click('button[type=submit]');
     const content = await page.content();
     expect(content).toContain('Email or password error');
   });
   it('Success at good mail and password', async () => {
-    await page.type('input[name=email]', 'my@email.com');
-    await page.type('input[name=password]', '1234');
+    await completeForm(page, {
+      email: 'my@email.com',
+      password: '1234',
+    });
     await page.click('button[type=submit]');
     const content = await page.content();
     expect(content).toContain('Projects');
   });
 });
+
+describe('Sign up', () => {
+  beforeEach(async () => {
+    await truncate(['User']);
+    await UserFactory({ email: 'my@email.com', password: '1234' });
+    await page.goto(`${APP}/sign-up`);
+    await page.waitForSelector('input[name=email]');
+    await page.waitForSelector('input[name=name]');
+    await page.waitForSelector('input[name=password]');
+    await page.waitForSelector('input[name=passwordR]');
+    await page.waitForSelector('button[type=submit]');
+    await clearInputs(page);
+  });
+  it('Fails on good form and not uniq email', async () => {
+    await completeForm(page, {
+      name: 'user',
+      email: 'no@email.com',
+      password: '1234',
+      passwordR: '1234',
+    });
+    await page.click('button[type=submit]');
+    const content = await page.content();
+    expect(content).toContain('Projects');
+  });
+  it('Success on good form and uniq email', async () => {
+    await completeForm(page, {
+      name: 'user',
+      email: 'my@email.com',
+      password: '1234',
+      passwordR: '1234',
+    });
+    await page.click('button[type=submit]');
+    const content = await page.content();
+    expect(content).toContain('Projects');
+  });
+});
+
