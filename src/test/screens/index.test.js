07cd64641dd7a7ee2c052524c6d37a932cca6ca0
Gabriel Ulloa <gsulloa@uc.cl>
2018-06-26 02:26:47 -0400

    passing all tests local

--- src/test/screens/index.test.js
+++ src/test/screens/index.test.js
@@ -3,14 +3,6 @@ const truncate = require('../models/truncate');
 const UserFactory = require('../factories/user');
 require('../../../index');
 
-async function clearInputs(page) {
-  await page.evaluate(() => document.querySelectorAll('input').forEach((n) => { n.value = ''; })); // eslint-disable-line
-}
-async function completeForm(page, data) {
-  for (const input in data) {
-    await page.type(`input[name=${input}]`, data[input]); // eslint-disable-line
-  }
-}
 function waitFor(timeToWait) {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -18,6 +10,22 @@ function waitFor(timeToWait) {
     }, timeToWait);
   });
 }
+const waitingTime = 1000;
+
+async function clearInputs(page) {
+  await page.evaluate(() => document.querySelectorAll('input').forEach((n) => { n.value = ''; })); // eslint-disable-line
+}
+async function completeForm(page, data) {
+  await waitFor(waitingTime);
+  for (const input in data) {
+    await page.type(`input[name=${input}]`, data[input]); // eslint-disable-line
+  }
+}
+async function clickSubmit(page) {
+  await page.click('button[type=submit]');
+  await waitFor(waitingTime);
+}
+
 
 const APP = 'http://localhost:3000';
 const timeout = 30000;
@@ -25,7 +33,7 @@ let page;
 let browser;
 
 const browserConfig = true ? {
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  // args: ['--no-sandbox', '--disable-setuid-sandbox'],
 } : {
   headless: false,
   slowMo: 10,
@@ -56,47 +64,41 @@ describe('Sign in', () => {
     await page.goto(`${APP}/sign-in`);
     await page.waitForSelector('input[name=email]');
     await page.waitForSelector('button[type=submit]');
-    await waitFor(5000);
     await clearInputs(page);
   }, timeout);
   it('Fails with mail and password error', async () => {
-    await waitFor(5000);
     await completeForm(page, {
       email: 'no@email.com',
       password: '12345',
     });
-    await page.click('button[type=submit]');
+    await clickSubmit(page);
     const content = await page.content();
     expect(content).toContain('Email or password error');
   }, timeout);
   it('Fails with mail error', async () => {
-    await waitFor(5000);
     await completeForm(page, {
       email: 'no@email.com',
       password: '1234',
     });
-    await page.click('button[type=submit]');
+    await clickSubmit(page);
     const content = await page.content();
     expect(content).toContain('Email or password error');
   }, timeout);
   it('Fails with password error', async () => {
-    await waitFor(5000);
     await completeForm(page, {
       email: 'my@email.com',
       password: '12345',
     });
-    await page.click('button[type=submit]');
+    await clickSubmit(page);
     const content = await page.content();
     expect(content).toContain('Email or password error');
   }, timeout);
   it('Success at good mail and password', async () => {
-    await waitFor(5000);
     await completeForm(page, {
       email: 'my@email.com',
       password: '1234',
     });
-    await page.click('button[type=submit]');
-    await waitFor(5000);
+    await clickSubmit(page);
     const content = await page.content();
     expect(content).toContain('Projects');
   }, timeout);
@@ -112,31 +114,28 @@ describe('Sign up', () => {
     await page.waitForSelector('input[name=password]');
     await page.waitForSelector('input[name=passwordR]');
     await page.waitForSelector('button[type=submit]');
-    await waitFor(5000);
     await clearInputs(page);
   }, timeout);
   it('Fails on good form and not uniq email', async () => {
-    await waitFor(5000);
     await completeForm(page, {
       name: 'user',
       email: 'my@email.com',
       password: '1234',
       passwordR: '1234',
     });
-    await page.click('button[type=submit]');
-    await waitFor(5000);
+    await clickSubmit(page);
     const content = await page.content();
     expect(content).toContain('email must be uniq');
   }, timeout);
   it('Success on good form and uniq email', async () => {
-    await waitFor(5000);
+    await waitFor(waitingTime);
     await completeForm(page, {
       name: 'user',
       email: 'new@email.com',
       password: '1234',
       passwordR: '1234',
     });
-    await page.click('button[type=submit]');
+    await clickSubmit(page);
     const content = await page.content();
     expect(content).toContain('Projects');
   }, timeout);
